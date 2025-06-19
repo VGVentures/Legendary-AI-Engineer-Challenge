cc51471145c2de1067484bf9f99687a941b35949
aksana-toyota <aksana.buster@woven-planet.global>
2025-06-19 03:38:09 -0500

    Added slight camera tilt to showcase beautiful planetary rings and closer zoom for immersive initial view

--- frontend/src/app/components/SpaceScene.tsx
+++ frontend/src/app/components/SpaceScene.tsx
@@ -267,11 +267,11 @@ export default function SpaceScene() {
   const getResponsiveCameraConfig = () => {
     switch (screenSize) {
       case 'mobile':
-        return { position: [0, 0, 15] as [number, number, number], fov: 70 };
+        return { position: [0, 2, 12] as [number, number, number], fov: 70 };
       case 'tablet':
-        return { position: [0, 0, 18] as [number, number, number], fov: 72 };
+        return { position: [0, 2.5, 15] as [number, number, number], fov: 72 };
       default: // desktop
-        return { position: [0, 0, 20] as [number, number, number], fov: 75 };
+        return { position: [0, 3, 17] as [number, number, number], fov: 75 };
     }
   };
 
